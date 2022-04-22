d33b8e8ae815bdce406155361591631d58690aad
Karthik Soman <karthiksoman@leo.ucsf.edu>
2022-04-21 22:28:04 -0700

    adding script for adding tables

---
+++ nasa_api/bin/create_tables.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text
+from nasa_api import db
+
+Base = declarative_base()
+
+print('Creating column_mapping table ...')
+class columnMapping(Base):
+    __tablename__ = 'column_mapping'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    study_id = Column(String(15), nullable=False)
+    column_name = Column(Text, nullable=False)
+    column_name_index = Column(String(15), nullable=False) 
+    
+Base.metadata.create_all(db.engine(), checkfirst=True)
+print('column_mapping table is created.')
