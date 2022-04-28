e53fac5c415347a308a1ec5dc848d1f831ed9832
Karthik Soman <karthiksoman@leo.ucsf.edu>
2022-04-27 23:38:15 -0700

    adding swagger

--- nasa_api/__init__.py
+++ nasa_api/__init__.py
@@ -1,17 +1,30 @@
 import flask
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 from nasa_api import db
 
+app_name = "nasa_spokesig_api"
+app = flask.Flask(app_name)
 
-app = flask.Flask("nasa_spokesig_api", static_folder=None)
+# app configs
 app.config['SQLALCHEMY_DATABASE_URI'] = db.db_url()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_SORT_KEYS'] = False
+
+# swagger configs
+SWAGGER_URL = "/nasa_api/v1/swagger"
+API_URL = "/static/swagger.yaml"
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        "app_name" : app_name
+    }
+)
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
+    
 flask_db = SQLAlchemy(app)
-
-
-
 
 def problem_response(message=None, details=None, status_code=422):
     problem = {}
